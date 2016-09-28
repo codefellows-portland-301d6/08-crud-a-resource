873f36d4a032f6d2c355908ea9a5c6772c731eed
JPGothro <jpgothro@gmail.com>
2016-09-28 13:47:10 -0700

    added SQL command to DELETE a row using the id

--- starter-code/scripts/models/article.js
+++ starter-code/scripts/models/article.js
@@ -98,7 +98,7 @@
           // TODO: Delete an article instance from the database based on its id:
           /* Note: this is an advanced admin option, so you will need to test
               out an individual query in the console */
-          'sql': '', // <--- complete the command here, inside the quotes;
+          'sql': 'DELETE FROM articles WHERE id = ?', // <--- complete the command here, inside the quotes;
           'data': [this.id]
         }
       ]
